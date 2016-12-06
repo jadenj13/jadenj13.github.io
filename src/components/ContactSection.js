0b887a10ce78032b6c909ddad928f9b7cfa90d43
jadenj13 <jadenj13@hotmail.com>
2016-12-05 22:34:23 -0700

    email working with formspree

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -26,11 +26,11 @@ export default class Contact extends React.Component {
 			<div className="contact-main">
 				<h3>interested? send me a message</h3>
 				<div className="contact-underline"></div>
-				<form>
-					<input type="text" placeholder="name" onChange={this.handleChange.bind(this, 'name')} />
-					<input type="email" placeholder="your email" onChange={this.handleChange.bind(this, 'email')} />
-					<textarea type="text" placeholder="message" onChange={this.handleChange.bind(this, 'body')} />
-					<button>send</button>
+				<form method="post" action="https://formspree.io/jadenj133@gmail.com">
+					<input type="text" placeholder="name" name="name" onChange={this.handleChange.bind(this, 'name')} className="contact-main-input" required />
+					<input type="email" placeholder="your email" name="email" onChange={this.handleChange.bind(this, 'email')} className="contact-main-input" required />
+					<textarea type="text" placeholder="message" name="message" onChange={this.handleChange.bind(this, 'body')} className="contact-main-input" required />
+					<input type="submit" value="send" className="send-btn" />
 				</form>
 			</div>
 		);
