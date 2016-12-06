17b82a4443993da89015f2724e363450261479fb
jadenj13 <jadenj13@hotmail.com>
2016-12-05 23:05:17 -0700

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,11 @@ import '../styles/footerStyles.css';
 export default function(props) {
 	return (
 		<div className="footer-main">
-
+			<div className="icon-container">
+				<a href="http://www.github.com/jadenj13"><div className="github-icon"></div></a>
+				<a href="https://ca.linkedin.com/in/jaden-johnson-706489123"><div className="linkedin-icon"></div></a>
+			</div>
+			<p>Copyright &copy; 2016 by Jaden Johnson. All rights reserved.</p>
 		</div>
 	)
 }
